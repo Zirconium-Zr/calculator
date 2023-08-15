42f008a116071fdebc7244350f72332306f21158
Potassium <brainiacgamer444@gmail.com>
2023-08-15 17:01:49 +0545

    Make function to display operator and previous value on screen

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const operandButtons = document.querySelectorAll("[operands]");
+const operatorButtons = document.querySelectorAll("[operators]");
 const currentTextField = document.querySelector(".big-text");
+const previousTextField = document.querySelector(".small-text");
 let firstNumber;
 let secondNumber;
 let operator;
@@ -43,6 +45,12 @@ function displayOutput(){
     operandButtons.forEach((button)=>{
         button.addEventListener("click", ()=>{
             currentTextField.textContent = currentTextField.textContent == "0" ? button.textContent : currentTextField.textContent += button.textContent;
+        })
+    })
+
+    operatorButtons.forEach((button)=>{
+        button.addEventListener("click", ()=>{
+            previousTextField.textContent = currentTextField.textContent + button.textContent;
         })
     })
 }
