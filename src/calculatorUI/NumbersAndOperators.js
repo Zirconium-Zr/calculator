ce4a382830dfcd5552bec68e078467d8c0244f26
Potassium <brainiacgamer444@gmail.com>
2024-01-17 20:45:05 +0545

    Display numbers on screen

--- src/calculatorUI/NumbersAndOperators.js
+++ src/calculatorUI/NumbersAndOperators.js
@@ -1,7 +1,15 @@
-import { bigTextField } from "./dom.js";
+import { bigTextField, smallTextField } from "./dom.js";
 
 export function displayNumbers(number) {
-  bigTextField.textContent = number;
+  // Do not let decimal appear more than once
+  if (number === "." && bigTextField.textContent.includes(".")) return;
+
+  // Replace initial zero with clicked number except when user clicks on decimal button
+  if (bigTextField.textContent === "0" && number !== ".") {
+    bigTextField.textContent = number;
+  } else {
+    bigTextField.textContent += number;
+  }
 }
 
 export function displayOperators(operator) {
--- src/calculatorUI/dom.js
+++ src/calculatorUI/dom.js
@@ -2,7 +2,7 @@ import { displayNumbers, displayOperators } from "./NumbersAndOperators.js";
 
 const buttons = document.querySelectorAll("button");
 export const bigTextField = document.querySelector(".big-text");
-const smallTextField = document.querySelector(".small-text");
+export const smallTextField = document.querySelector(".small-text");
 
 window.onload = () => {
   bigTextField.textContent = "0";
@@ -10,7 +10,7 @@ window.onload = () => {
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (button.classList.contains("number")) {
+    if (button.classList.contains("number") || button.classList.contains("dot")) {
       displayNumbers(button.textContent);
     } else if (button.classList.contains("operator")) {
       displayOperators(button.textContent);
