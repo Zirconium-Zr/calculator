f1d51eb57bf7d32c8e8b130bda5858261ab55a0b
Potassium <brainiacgamer444@gmail.com>
2024-01-17 21:16:45 +0545

    Write logic for numbers to display after operator is selected

--- src/calculatorUI/NumbersAndOperators.js
+++ src/calculatorUI/NumbersAndOperators.js
@@ -1,17 +1,31 @@
 import { bigTextField, smallTextField } from "./dom.js";
 
+let newInput = false;
+
 export function displayNumbers(number) {
+  // Replace bigTextField with zero and decimal if user clicks on decimal after selecting an operator
+  if (number === "." && newInput) {
+    bigTextField.textContent = `0${number}`;
+    newInput = false;
+    return;
+  }
+
   // Do not let decimal appear more than once
   if (number === "." && bigTextField.textContent.includes(".")) return;
 
-  // Replace initial zero with clicked number except when user clicks on decimal button
+  // Replace initial zero with clicked number except when user clicks on decimal button or zero
   if (bigTextField.textContent === "0" && number !== ".") {
     bigTextField.textContent = number;
   } else {
-    bigTextField.textContent += number;
+    // Replace bigTextField values with new input values if user clicks on any number buttons except decimal after selecting an operator
+    if (newInput) {
+      bigTextField.textContent = number;
+      newInput = false;
+    } else bigTextField.textContent += number;
   }
 }
 
 export function displayOperators(operator) {
-  bigTextField.textContent = operator;
+  newInput = true;
+  smallTextField.textContent = `${bigTextField.textContent} ${operator}`;
 }
