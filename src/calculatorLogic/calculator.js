b05faba68b905cf392a84ff1cc3ff27eb567eaa1
Potassium <brainiacgamer444@gmail.com>
2024-01-18 22:20:06 +0545

    Create condition to replace second number with the initial answer if operator is selected right after clicking equals to sign and the equal sign is clicked again

--- src/calculatorLogic/calculator.js
+++ src/calculatorLogic/calculator.js
@@ -31,7 +31,9 @@ export let firstNumber = "0",
   operatorSign = "",
   answer = "";
 
-let switchToSecondNumber = false;
+let switchToSecondNumber = false,
+  displayedAnswer = false,
+  resetSecondNumber = false;
 
 export function assignOperands(operand) {
   if (!switchToSecondNumber) firstNumber = operand;
@@ -43,14 +45,25 @@ export function assignOperands(operand) {
 export function assignOperator(operator) {
   operatorSign = convertOperatorSign(operator, "Calculator");
   switchToSecondNumber = true;
+
+  // If user selects operator while the answer is on the screen
+  if (displayedAnswer) {
+    displayedAnswer = false;
+    // Reset the second number so that the second number will be the new number that will be provided after selecting the operator. However, the second number will be replaced after clicking equals to sign
+    resetSecondNumber = true;
+  }
   console.log({ firstNumber, secondNumber, operatorSign, switchToSecondNumber });
   return { operatorSign };
 }
 
 export function getAnswer() {
   if (operatorSign === "" || operatorSign === "No operator") return firstNumber;
-  if (secondNumber === "") secondNumber = firstNumber;
+  if (secondNumber === "" || resetSecondNumber) {
+    secondNumber = firstNumber;
+    resetSecondNumber = false;
+  }
   switchToSecondNumber = false;
+  displayedAnswer = true;
   answer = operate(firstNumber, secondNumber, operatorSign);
   console.log({ firstNumber, secondNumber, operatorSign, switchToSecondNumber, answer });
   return answer;
